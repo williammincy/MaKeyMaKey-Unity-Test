7c13ca6c2778c50f8bbdb3326780f263d16186e3
William Mincy <will@wmdigital.com>
2015-02-22 15:13:08 -0500

    + Initial commit of Unity3D test project

---
+++ Assets/Standard Assets/Scripts/General Scripts/KeyPressDetection.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyPressDetection : MonoBehaviour {
+	// list to hold all of the objects we are manipulating
+	private List<GameObject> fills;
+
+	// Use this for initialization
+	void Start () {
+		// collect all the objects
+		fills = new List<GameObject>();
+		fills.Add(GameObject.Find("GO_Up_Fill"));
+		fills.Add(GameObject.Find("GO_Down_Fill"));
+		fills.Add(GameObject.Find("GO_Left_Fill"));
+		fills.Add(GameObject.Find("GO_Right_Fill"));
+		fills.Add(GameObject.Find("GO_W_Fill"));
+		fills.Add(GameObject.Find("GO_A_Fill"));
+		fills.Add(GameObject.Find("GO_S_Fill"));
+		fills.Add(GameObject.Find("GO_D_Fill"));
+		HideFills();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateFills();
+	}
+
+	/**
+	 * Iterates through the <code>fills</code> list to hide all of the objects within the collection
+	 * @return void
+	 */
+	void HideFills() {
+		foreach(GameObject go in fills) {
+			go.SetActive(false);
+		}
+	}
+	/**
+	 * Evaluates each key that we are listening for against its corresponding <code>GameObject</code> to determine if we should 
+	 * see the object. When the associating key is pressed the object is visible, otherwise the object is not visible
+	 * @return void
+	 */
+	void UpdateFills() {
+		fills[0].SetActive(Input.GetKey(KeyCode.UpArrow));
+		fills[1].SetActive(Input.GetKey(KeyCode.DownArrow));
+		fills[2].SetActive(Input.GetKey(KeyCode.LeftArrow));
+		fills[3].SetActive(Input.GetKey(KeyCode.RightArrow));
+		fills[4].SetActive(Input.GetKey(KeyCode.W));
+		fills[5].SetActive(Input.GetKey(KeyCode.A));
+		fills[6].SetActive(Input.GetKey(KeyCode.S));
+		fills[7].SetActive(Input.GetKey(KeyCode.D));
+	}
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# MaKeyMaKey-Unity-Test
+A simple C# Unity Application to test MaKey Makey within Unity made in Unity 4.3. I have included a published Windows executable along with the source code, just in case someone may want to run this on a Windows machine without having to compile.
+
+The application assumes that you have remapped the MaKey Makey with the following keys
+* Up
+* DOwn
+* Left 
+* Right
+* W
+* A 
+* S 
+* D 
+
+To remap the MaKey MaKey go to this URL: http://www.makeymakey.com/remap/
+
+When running the application with a MaKey MaKey connected the corresponding graphic will light up when its key is pressed. It will go back to a default state when the key is released.
